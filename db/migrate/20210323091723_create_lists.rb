af730306a8adc1753fe6c96b6421a60fcc3ca8e0
EC2 Default User <ec2-user@ip-172-31-63-171.ec2.internal>
2021-03-24 01:43:42 +0000

    [Update]migaret変更

--- db/migrate/20210323091723_create_lists.rb
+++ db/migrate/20210323091723_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
 
-      t.string :tile
+      t.string :title
       t.string :body
 
       t.timestamps
